376a8ad8569db5a7c9aad14f0423b18dca7ec662
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-07-12 15:30:40 +0530

    feat: connect express

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,4 +15,16 @@ app.use(express.json({limit: "16kb"}))
 app.use(urlencoded({extended:true, limit: "16kb"}))
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.send("App is connected")
+})
+
+const port = process.env.PORT || 3000
+
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`)
+})
+
+
 export { app }
