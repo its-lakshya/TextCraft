8b81d8171d6b5ba6ac7a969dad4c59ad01c5ff07
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-07-18 01:19:50 +0530

    feat: install framer-motion and add animation on landing section

--- frontend/src/components/home/Landing.tsx
+++ frontend/src/components/home/Landing.tsx
@@ -1,19 +1,32 @@
 import { buttonHoverAnimaiton } from '../../utils/TailwindUtils';
 import { FaArrowRightLong } from 'react-icons/fa6';
-import LandingPageBackground from "../../assets/images/LandingPageBackground.svg"
-import LandingPageImage from "../../assets/images/LandingPageImage.avif"
+import LandingPageBackground from '../../assets/images/LandingPageBackground.svg';
+import LandingPageImage from '../../assets/images/LandingPageImage.avif';
+import { motion } from 'framer-motion';
 
 const Landing = () => {
+  const text = 'Unite Ideas. Write Together.'.split('');
   return (
-    <div className={`LANDING-PAGE flex flex-col justify-center items-center gap-20 w-screen h-auto py-20`}
-    style={{ backgroundImage: `url(${LandingPageBackground})` }}>
+    <div
+      className={`LANDING-PAGE flex flex-col justify-center items-center gap-20 w-screen h-auto py-20`}
+      style={{ backgroundImage: `url(${LandingPageBackground})` }}
+    >
       <div className="TEXT-SECTION flex flex-col justify-center items-center gap-10">
         <span className="TAG-LINE text-[5vw] leading-none text-black font-extrabold">
-          Unite Ideas. Write Together.
+          {text.map((letter, index) => (
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: index / 20 }}
+              key={index}
+            >
+              {letter}
+            </motion.span>
+          ))}
         </span>
         <span className="DESCRIPTION w-[60vw] text-center text-[18px]">
-          Designed for teams to effortlessly create and innovate together, enhance
-          productivity in every document. <br/>
+          Designed for teams to effortlessly create and innovate together, enhance productivity in
+          every document. <br />
           Streamline teamwork with synchronized edits and real-time feedback.
         </span>
         <button
@@ -26,10 +39,11 @@ const Landing = () => {
           </span>
         </button>
       </div>
-      <div className='IMAGE-CONTAINER flex justify-center items-center w-[78vw] h-[50vw] bg-white rounded-[20px] border-[6px] border-primaryDark'
-      >
-        <img src={LandingPageImage} alt='img' className='w-full'/>
-      </div>
+      <motion.div initial={{ opacity: 0, marginTop: '20rem' }}
+                            animate={{ opacity: 1, marginTop: "0" }}
+                            exit={{ opacity: 0, scale: 0 }} transition={{duration: 1}} className="IMAGE-CONTAINER flex justify-center items-center w-[78vw] h-[50vw] bg-white rounded-[20px] border-[6px] border-primaryDark">
+        <img src={LandingPageImage} alt="img" className="w-full" />
+      </motion.div >
     </div>
   );
 };
