983c74be8b88cd5902cc782a0d3d0221cc66df18
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-07-20 22:11:25 +0530

    feat: enhance the animation of create document

---
+++ frontend/src/components/documents/CreateDocument.tsx
@@ -0,0 +1,32 @@
+import { motion } from 'framer-motion';
+import { MdPlaylistAdd } from 'react-icons/md';
+
+const CreateDocument: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center gap-[10vw] mt-[4rem] w-full h-[20rem] max-h-[20rem] bg-primaryDark text-white">
+      <motion.button
+        className="CREATE-DOCUMENT flex justify-center items-center w-36 h-48 bg-white text-6xl text-[#EA4336]"
+        initial={{ scale: 100 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 0.5, ease: 'easeIn' }}
+        whileHover={{ scale: 1.1 }}
+      >
+        <motion.span
+          initial={{ scale: 0.6 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5, ease: 'easeIn', delay: 0.5 }}
+        >
+          <MdPlaylistAdd />
+        </motion.span>
+      </motion.button>
+      <div className="TEXT flex flex-col justify-center items-center h-full">
+        <div className="HEADING flex gap-0 text-5xl text-white font-semibold">
+          Just click and create
+          <span className="animate-bounce text-6xl">!</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateDocument;
