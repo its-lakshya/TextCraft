d440df0b19014782eb20f439fcd0bea1ba4d492c
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-07-22 00:16:10 +0530

    feat: add auth check to display login and signup buttons from header

--- frontend/src/layouts/header/Header.tsx
+++ frontend/src/layouts/header/Header.tsx
@@ -1,10 +1,36 @@
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { buttonHoverAnimaiton } from '../../utils/TailwindUtils';
-// import { useEffect, useState } from 'react';
+import { buttonHoverAnimaiton } from '../../utils/Tailwind.utils';
+import { useEffect, useRef } from 'react';
+import { isAuthenticated } from '../../utils/Auth.utils';
 
 const Header: React.FC = () => {
   const location = useLocation();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const loginRef = useRef<HTMLDivElement>(null);
+
+  const hideLoginSiginButton = (): void => {
+    if (loginRef.current) {
+      loginRef.current.style.display = 'none';
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem('isAuthenticated') !== 'true') {
+      (async () => {
+        try {
+          const isLoggedIn = await isAuthenticated();
+          if (!isLoggedIn) {
+            hideLoginSiginButton();
+          }
+        } catch (error) {
+          console.error(error, 'Error checking authentication');
+          hideLoginSiginButton();
+        }
+      })();
+    } else {
+      hideLoginSiginButton();
+    }
+  }, []);
 
   const currentLocation = location.pathname.split('/');
   if (currentLocation[1] === 'document' || currentLocation[1] === 'documents') return null;
@@ -19,17 +45,19 @@ const Header: React.FC = () => {
             Text<span className="text-primary">Craft</span>
           </span>
         </button>
-        <Link to='/contact-us' className="CONTACT US">Contact us</Link>
+        <Link to="/contact-us" className="CONTACT US">
+          Contact us
+        </Link>
       </div>
-      <div className="HEADER-RIGHT flex justify-center items-center gap-4 w-auto">
+      <div ref={loginRef} className="HEADER-RIGHT flex justify-center items-center gap-4 w-auto">
         <Link
-          to='/auth/login'
+          to="/auth/login"
           className={`LOGIN text-primary px-6 py-3 rounded-full ${buttonHoverAnimaiton} hover:bg-primaryLight`}
         >
           Log in
         </Link>
         <Link
-          to='/auth/register'
+          to="/auth/register"
           className={`SIGNUP bg-primary text-white px-8 py-3 rounded-full ${buttonHoverAnimaiton} hover:bg-primaryDark`}
         >
           Sign up now
