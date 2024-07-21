c2bacdb3fce9759ba66d0ca99b2caabb9fd4790b
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-07-21 23:52:08 +0530

    feat: integrate login api

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,17 +1,50 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import AuthBackgroundImage from '../assets/images/AuthBackgroundImage.svg';
-import { buttonHoverAnimaiton } from '../utils/TailwindUtils';
+import { buttonHoverAnimaiton } from '../utils/Tailwind.utils';
+import { useState } from 'react';
+import axios from '../axios.config';
+
+type User = {
+  email: string;
+  password: string;
+};
 
 const Login: React.FC = () => {
-  
-  const inputStyles: string = 'w-full h-8 rounded-md px-4 border border-[#BCBEC0] text-black text-sm';
+  const navigate = useNavigate();
+  const inputStyles: string =
+    'w-full h-8 rounded-md px-4 border border-[#BCBEC0] text-black text-sm';
   const inputContainerStyles: string = 'flex flex-col gap-2 w-full';
+  const [user, setUser] = useState<User>({ email: '', password: '' });
+
+  const handleUserName = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setUser({ ...user, email: e.target.value });
+  };
+
+  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setUser({ ...user, password: e.target.value });
+  };
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/users/login', {
+        email: user.email,
+        password: user.password,
+      });
+      console.log(response);
+      localStorage.setItem('isAuthenticated', 'true');
+      navigate('/');
+    } catch (error) {
+      console.log(error, 'Logged in unsuccessfull');
+    }
+  };
+
   return (
     <div
       className="WRAPPER relative flex justify-center items-center w-full h-screen bg-center bg-cover text-white"
       style={{ background: 'linear-gradient(180deg, rgba(0,0,0,1) 11%, rgba(76,29,149,1) 99%)' }}
     >
-      <img src={AuthBackgroundImage} alt='img' className='absolute'/>
+      <img src={AuthBackgroundImage} alt="img" className="absolute" />
       <div
         className="MAIN-CONTAINER flex flex-col justify-center items-center gap-10 w-[450px] h-auto p-10 px-rootXPadd border border-primary rounded-3xl"
         style={{ background: 'rgba(88, 130, 193, 0.28)', backdropFilter: 'blur(8.89323px)' }}
@@ -23,13 +56,16 @@ const Login: React.FC = () => {
         </button>
         <div className="FORM-CONTAINER flex flex-col gap-6 w-full">
           <div className="HEADING text-2xl font-bold">Login</div>
-          <form className="FORM flex flex-col gap-6">
+          <form className="FORM flex flex-col gap-6" onSubmit={e => handleLogin(e)} method="post">
             <div className="flex flex-col gap-3 items-start w-full">
               <div className={`${inputContainerStyles}`}>
                 <label className="EMAIL-LABEL capitalize">Email</label>
                 <input
                   type="text"
                   placeholder="username@gmail.com"
+                  onChange={e => {
+                    handleUserName(e);
+                  }}
                   className={`EMAIL-INPUT ${inputStyles}`}
                 />
               </div>
@@ -38,17 +74,24 @@ const Login: React.FC = () => {
                 <input
                   type="password"
                   placeholder="password"
+                  onChange={e => handlePassword(e)}
                   className={`PASSWORD-INPUT ${inputStyles}`}
                 />
               </div>
               <button className="FORGOT-PASSWORD capitalize">Forgot Password?</button>
             </div>
-            <button className={`LOGIN-BUTTON flex justify-center items-center text-lg font-bold bg-primary py-2 rounded-lg ${buttonHoverAnimaiton} hover:-translate-y-2 hover:bg-primaryDark`}>
+            <button
+              type="submit"
+              className={`LOGIN-BUTTON flex justify-center items-center text-lg font-bold bg-primary py-2 rounded-lg ${buttonHoverAnimaiton} hover:-translate-y-2 hover:bg-primaryDark`}
+            >
               Login
             </button>
           </form>
           <span className="SIGN-UP text-xs text-center font-light">
-            Don't have an account yet? <Link to='/auth/register' className="font-bold">Register for free</Link>
+            Don't have an account yet?{' '}
+            <Link to="/auth/register" className="font-bold">
+              Register for free
+            </Link>
           </span>
         </div>
       </div>
