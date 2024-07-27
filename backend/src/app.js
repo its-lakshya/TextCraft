d5bd2581d51f93c2b03393995497f94f3519c8b9
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-07-27 17:21:35 +0530

    feat: add public routes to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.routes.js';
 import documentRouter from './routes/document.routes.js';
 import collaborationRouter from './routes/collaboration.routes.js';
 import favouriteRouter from './routes/favourite.routes.js';
+import publicRouter from './routes/public.routes.js';
 
 const app = express();
 
@@ -29,5 +30,6 @@ app.use('/api/v1/users', userRouter);
 app.use('/api/v1/documents', documentRouter);
 app.use('/api/v1/collaborations', collaborationRouter);
 app.use('/api/v1/favourite', favouriteRouter);
+app.use('/api/v1/public', publicRouter)
 
 export { app };
