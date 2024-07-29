4f66ba28215073a30d1c516b797ba9c4ebcc8ad8
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-07-30 01:00:58 +0530

    feat: add loader in login and register screens

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -4,6 +4,7 @@ import { buttonHoverAnimaiton } from '../utils/Tailwind.utils';
 import { useState } from 'react';
 import axios from '../axios.config';
 import { UserEmailPassword } from '../types/Global.types';
+import Loader from '../components/loader/Loader';
 
 const Login: React.FC = () => {
   const navigate = useNavigate();
@@ -14,9 +15,10 @@ const Login: React.FC = () => {
   const [invalidEmail, setInvalidEmail] = useState<boolean>(false);
   const [isLoginFailed, setIsLoginFailed] = useState<boolean>(false);
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleEmail = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setIsLoginFailed(false)
+    setIsLoginFailed(false);
     if (emailRegex.test(event.target.value) === true) {
       setInvalidEmail(false);
       setUser({ ...user, email: event.target.value });
@@ -24,13 +26,14 @@ const Login: React.FC = () => {
   };
 
   const handlePassword = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setIsLoginFailed(false)
+    setIsLoginFailed(false);
     setUser({ ...user, password: e.target.value });
   };
 
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
     if (!invalidEmail) {
+      setIsLoading(true);
       try {
         await axios.post('/users/login', {
           email: user.email,
@@ -42,70 +45,85 @@ const Login: React.FC = () => {
         console.log(error, 'Logged in unsuccessfull');
         setIsLoginFailed(true);
       }
+      setIsLoading(false);
     }
   };
 
-  return (
-    <div className="WRAPPER relative flex justify-center items-center w-full h-screen bg-center bg-cover text-white overflow-hidden bg-primaryDark">
-      <img src={AuthBackgroundImage} alt="img" className="absolute" />
-      <div
-        className="MAIN-CONTAINER flex flex-col justify-center items-center gap-10 w-[450px] h-auto p-10 px-rootXPadd border border-primary rounded-3xl"
-        style={{ background: 'rgba(88, 130, 193, 0.28)', backdropFilter: 'blur(8.89323px)' }}
-      >
-        <button className="LOGO text-logoFontSize font-bold mr-4 drop-shadow-lg">
-          <span className="text-primaryDark">
-            Text<span className="text-primary">Craft</span>
-          </span>
-        </button>
-        {isLoginFailed ? <div className='absolute top-24 text-sm text-red-500'>Login failed, either passwor or email is incorrect</div> : null }
-        <div className="FORM-CONTAINER flex flex-col gap-6 w-full">
-          <div className="HEADING text-2xl font-bold">Login</div>
-          <form className="FORM flex flex-col gap-6" onSubmit={e => handleLogin(e)} method="post">
-            <div className="flex flex-col gap-3 items-start w-full">
-              <div className={`relative ${inputContainerStyles}`}>
-                <label className="EMAIL-LABEL capitalize">Email</label>
-                <input
-                  type="text"
-                  placeholder="username@gmail.com"
-                  required
-                  onChange={e => {
-                    handleEmail(e);
-                  }}
-                  className={`EMAIL-INPUT ${inputStyles}`}
-                />
-                {invalidEmail ? (
-                  <div className="absolute top-16 right-0 text-xs text-red-500">Invalid Email</div>
-                ) : null}
-              </div>
-              <div className={`${inputContainerStyles}`}>
-                <label className="PASSWORD-LABEL capitalize">Password</label>
-                <input
-                  type="password"
-                  placeholder="password"
-                  required
-                  onChange={e => handlePassword(e)}
-                  className={`PASSWORD-INPUT ${inputStyles}`}
-                />
-              </div>
-              <button className="FORGOT-PASSWORD capitalize">Forgot Password?</button>
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center w-screen h-screen">
+        <Loader />
+      </div>
+    );
+  } else {
+    return (
+      <div className="WRAPPER relative flex justify-center items-center w-full h-screen bg-center bg-cover text-white overflow-hidden bg-primaryDark">
+        <img src={AuthBackgroundImage} alt="img" className="absolute" />
+        <div
+          className="MAIN-CONTAINER flex flex-col justify-center items-center gap-10 w-[450px] h-auto p-10 px-rootXPadd border border-primary rounded-3xl"
+          style={{ background: 'rgba(88, 130, 193, 0.28)', backdropFilter: 'blur(8.89323px)' }}
+        >
+          <button className="LOGO text-logoFontSize font-bold mr-4 drop-shadow-lg">
+            <span className="text-primaryDark">
+              Text<span className="text-primary">Craft</span>
+            </span>
+          </button>
+          {isLoginFailed ? (
+            <div className="absolute top-24 text-sm text-red-500">
+              Login failed, either passwor or email is incorrect
             </div>
-            <button
-              type="submit"
-              className={`LOGIN-BUTTON flex justify-center items-center text-lg font-bold bg-primary py-2 rounded-lg ${buttonHoverAnimaiton} hover:-translate-y-2 hover:bg-primaryDark ${invalidEmail ? 'pointer-events-none' : null}`}
-            >
-              Login
-            </button>
-          </form>
-          <span className="SIGN-UP text-xs text-center font-light">
-            Don't have an account yet?{' '}
-            <Link to="/auth/register" className="font-bold">
-              Register for free
-            </Link>
-          </span>
+          ) : null}
+          <div className="FORM-CONTAINER flex flex-col gap-6 w-full">
+            <div className="HEADING text-2xl font-bold">Login</div>
+            <form className="FORM flex flex-col gap-6" onSubmit={e => handleLogin(e)} method="post">
+              <div className="flex flex-col gap-3 items-start w-full">
+                <div className={`relative ${inputContainerStyles}`}>
+                  <label className="EMAIL-LABEL capitalize">Email</label>
+                  <input
+                    type="text"
+                    placeholder="username@gmail.com"
+                    required
+                    onChange={e => {
+                      handleEmail(e);
+                    }}
+                    className={`EMAIL-INPUT ${inputStyles}`}
+                  />
+                  {invalidEmail ? (
+                    <div className="absolute top-16 right-0 text-xs text-red-500">
+                      Invalid Email
+                    </div>
+                  ) : null}
+                </div>
+                <div className={`${inputContainerStyles}`}>
+                  <label className="PASSWORD-LABEL capitalize">Password</label>
+                  <input
+                    type="password"
+                    placeholder="password"
+                    required
+                    onChange={e => handlePassword(e)}
+                    className={`PASSWORD-INPUT ${inputStyles}`}
+                  />
+                </div>
+                <button className="FORGOT-PASSWORD capitalize">Forgot Password?</button>
+              </div>
+              <button
+                type="submit"
+                className={`LOGIN-BUTTON flex justify-center items-center text-lg font-bold bg-primary py-2 rounded-lg ${buttonHoverAnimaiton} hover:-translate-y-2 hover:bg-primaryDark ${invalidEmail ? 'pointer-events-none' : null}`}
+              >
+                Login
+              </button>
+            </form>
+            <span className="SIGN-UP text-xs text-center font-light">
+              Don't have an account yet?{' '}
+              <Link to="/auth/register" className="font-bold">
+                Register for free
+              </Link>
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Login;
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -3,6 +3,7 @@ import AuthBackgroundImage from '../assets/images/AuthBackgroundImage.svg';
 import { buttonHoverAnimaiton } from '../utils/Tailwind.utils';
 import { useState } from 'react';
 import axios from '../axios.config';
+import Loader from '../components/loader/Loader';
 
 type User = {
   fullName: string;
@@ -19,6 +20,7 @@ const Register: React.FC = () => {
   const inputContainerStyles: string = 'flex flex-col gap-2 w-full';
   const [invalidEmail, setInvalidEmail] = useState<boolean>(false);
   const [invalidNumber, setInvalidNumber] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const indianMobileNumberRegex = /^[6789]\d{9}$/;
 
@@ -51,6 +53,7 @@ const Register: React.FC = () => {
   const handleRegister = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
     if (!invalidEmail && !invalidNumber) {
+      setIsLoading(true);
       const userData = {
         fullName: user.fullName,
         userName: user.userName,
@@ -65,115 +68,124 @@ const Register: React.FC = () => {
       } catch (error) {
         console.log(error, 'Not registered');
       }
+      setIsLoading(false);
     }
   };
 
-  return (
-    <div
-      className="WRAPPER flex justify-center items-center w-full h-screen bg-center bg-cover bg-primaryDark text-white"
-      style={{ backgroundImage: `url(${AuthBackgroundImage})` }}
-    >
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center w-screen h-screen">
+        <Loader />
+      </div>
+    );
+  } else {
+    return (
       <div
-        className="MAIN-CONTAINER flex flex-col justify-center items-center gap-10 w-[450px] h-auto p-10 px-rootXPadd border border-primary rounded-3xl"
-        style={{ background: 'rgba(88, 130, 193, 0.28)', backdropFilter: 'blur(8.89323px)' }}
+        className="WRAPPER flex justify-center items-center w-full h-screen bg-center bg-cover bg-primaryDark text-white"
+        style={{ backgroundImage: `url(${AuthBackgroundImage})` }}
       >
-        <button className="LOGO text-logoFontSize font-bold mr-4 drop-shadow-lg">
-          <span className="text-primaryDark">
-            Text<span className="text-primary">Craft</span>
-          </span>
-        </button>
-        <div className="FORM-CONTAINER flex flex-col gap-6 w-full">
-          <div className="HEADING text-2xl font-bold">Register</div>
-          <form
-            className="FORM flex flex-col gap-6"
-            onSubmit={e => handleRegister(e)}
-            method="post"
-          >
-            <div className="flex flex-col gap-3 items-start w-full">
-              <div className={`${inputContainerStyles}`}>
-                <label className="NAME-LABEL capitalize">Fullname</label>
-                <input
-                  type="text"
-                  name="fullName"
-                  placeholder="John Doe"
-                  required
-                  className={`NAME-INPUT ${inputStyles}`}
-                  onChange={e => handleInputChange(e)}
-                />
-              </div>
-              <div className={`${inputContainerStyles}`}>
-                <label className="EMAIL-LABEL capitalize">username</label>
-                <input
-                  type="text"
-                  name="userName"
-                  placeholder="john@doe"
-                  required
-                  className={`EMAIL-INPUT ${inputStyles}`}
-                  onChange={e => handleInputChange(e)}
-                />
-              </div>
-              <div className={`relative ${inputContainerStyles}`}>
-                <label className="MOBILE-LABEL capitalize">
-                  mobile <span className="text-xs">(optional)</span>
-                </label>
-                <input
-                  type="text"
-                  name="mobileNumber"
-                  placeholder="91XXXXXXXX"
-                  className={`MOBILE-INPUT ${inputStyles}`}
-                  onChange={e => handleInputChange(e)}
-                />
-                {invalidNumber ? (
-                  <div className="absolute top-[66px] right-0 text-xs text-red-500">
-                    Invalid mobile number
-                  </div>
-                ) : null}
-              </div>
-              <div className={`relative ${inputContainerStyles}`}>
-                <label className="EMAIL-LABEL capitalize">email</label>
-                <input
-                  type="text"
-                  name="email"
-                  required
-                  placeholder="username@gmail.com"
-                  className={`EMAIL-INPUT ${inputStyles}`}
-                  onChange={e => handleInputChange(e)}
-                />
-                {invalidEmail ? (
-                  <div className="absolute top-[66px] right-0 text-xs text-red-500">
-                    Invalid email
-                  </div>
-                ) : null}
-              </div>
-              <div className={`${inputContainerStyles}`}>
-                <label className="PASSWORD-LABEL capitalize">Password</label>
-                <input
-                  type="password"
-                  name="password"
-                  required
-                  placeholder="password"
-                  className={`PASSWORD-INPUT ${inputStyles}`}
-                  onChange={e => handleInputChange(e)}
-                />
-              </div>
-            </div>
-            <button
-              type="submit"
-              className={`LOGIN-BUTTON flex justify-center items-center text-lg font-bold bg-primary py-2 rounded-lg ${buttonHoverAnimaiton} hover:-translate-y-2 hover:bg-primaryDark ${invalidEmail ? 'pointer-events-none' : null}`}
+        <div
+          className="MAIN-CONTAINER flex flex-col justify-center items-center gap-10 w-[450px] h-auto p-10 px-rootXPadd border border-primary rounded-3xl"
+          style={{ background: 'rgba(88, 130, 193, 0.28)', backdropFilter: 'blur(8.89323px)' }}
+        >
+          <button className="LOGO text-logoFontSize font-bold mr-4 drop-shadow-lg">
+            <span className="text-primaryDark">
+              Text<span className="text-primary">Craft</span>
+            </span>
+          </button>
+          <div className="FORM-CONTAINER flex flex-col gap-6 w-full">
+            <div className="HEADING text-2xl font-bold">Register</div>
+            <form
+              className="FORM flex flex-col gap-6"
+              onSubmit={e => handleRegister(e)}
+              method="post"
             >
-              Sign up
-            </button>
-          </form>
-          <span className="SIGN-UP text-xs text-center font-light">
-            Already have an account?{' '}
-            <Link to="/auth/login" className="font-bold">
-              Login
-            </Link>
-          </span>
+              <div className="flex flex-col gap-3 items-start w-full">
+                <div className={`${inputContainerStyles}`}>
+                  <label className="NAME-LABEL capitalize">Fullname</label>
+                  <input
+                    type="text"
+                    name="fullName"
+                    placeholder="John Doe"
+                    required
+                    className={`NAME-INPUT ${inputStyles}`}
+                    onChange={e => handleInputChange(e)}
+                  />
+                </div>
+                <div className={`${inputContainerStyles}`}>
+                  <label className="EMAIL-LABEL capitalize">username</label>
+                  <input
+                    type="text"
+                    name="userName"
+                    placeholder="john@doe"
+                    required
+                    className={`EMAIL-INPUT ${inputStyles}`}
+                    onChange={e => handleInputChange(e)}
+                  />
+                </div>
+                <div className={`relative ${inputContainerStyles}`}>
+                  <label className="MOBILE-LABEL capitalize">
+                    mobile <span className="text-xs">(optional)</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="mobileNumber"
+                    placeholder="91XXXXXXXX"
+                    className={`MOBILE-INPUT ${inputStyles}`}
+                    onChange={e => handleInputChange(e)}
+                  />
+                  {invalidNumber ? (
+                    <div className="absolute top-[66px] right-0 text-xs text-red-500">
+                      Invalid mobile number
+                    </div>
+                  ) : null}
+                </div>
+                <div className={`relative ${inputContainerStyles}`}>
+                  <label className="EMAIL-LABEL capitalize">email</label>
+                  <input
+                    type="text"
+                    name="email"
+                    required
+                    placeholder="username@gmail.com"
+                    className={`EMAIL-INPUT ${inputStyles}`}
+                    onChange={e => handleInputChange(e)}
+                  />
+                  {invalidEmail ? (
+                    <div className="absolute top-[66px] right-0 text-xs text-red-500">
+                      Invalid email
+                    </div>
+                  ) : null}
+                </div>
+                <div className={`${inputContainerStyles}`}>
+                  <label className="PASSWORD-LABEL capitalize">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    required
+                    placeholder="password"
+                    className={`PASSWORD-INPUT ${inputStyles}`}
+                    onChange={e => handleInputChange(e)}
+                  />
+                </div>
+              </div>
+              <button
+                type="submit"
+                className={`LOGIN-BUTTON flex justify-center items-center text-lg font-bold bg-primary py-2 rounded-lg ${buttonHoverAnimaiton} hover:-translate-y-2 hover:bg-primaryDark ${invalidEmail ? 'pointer-events-none' : null}`}
+              >
+                Sign up
+              </button>
+            </form>
+            <span className="SIGN-UP text-xs text-center font-light">
+              Already have an account?{' '}
+              <Link to="/auth/login" className="font-bold">
+                Login
+              </Link>
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Register;
